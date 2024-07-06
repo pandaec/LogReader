3b274f5c9549bbda4c2a031b96ab3b1523bc307a
pandaec <ericchan1026@gmail.com>
2024-07-06 15:07:51 +0800

    Add Log Parser query syntax to this project

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,16 +4,18 @@ import com.sun.net.httpserver.HttpServer;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpExchange;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -40,8 +42,9 @@ public class Main {
     static class SearchHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange exchange) throws IOException {
-            Map<String, String> queryMap = queryToMap(exchange.getRequestURI().getQuery());
-            String query = queryMap.get("q");
+            Map<String, String> queryMap = queryToMap(exchange.getRequestURI().getRawQuery());
+            String query = queryMap.get("q") != null ? URLDecoder.decode(queryMap.get("q"), StandardCharsets.UTF_8)
+                    : "";
             exchange.getResponseHeaders().add("Content-Type", "text/event-stream");
             exchange.getResponseHeaders().add("Cache-Control", "no-cache");
             exchange.getResponseHeaders().add("Connection", "keep-alive");
@@ -51,18 +54,22 @@ public class Main {
                 ILogParser.Log log = ILogParser.Log.load(List.of(Paths.get("static", "dummy.log")));
                 log.start();
                 int i = 0;
+                Map<String, Pattern> patternMap = parseFilter(query, false);
+
                 for (ILogParser.LogDetail line : log.lines) {
-                    if(i++ > 500) {
-                        break;
-                    }
-                    if (line.getContent().contains(query)) {
-                        String data = "<div>" + escapeXml(line.priority + ";" + line.threadName + ";" + line.time + ";" + line.getContent()) + "</div>";
+                    if (isLineMatchFilter(line, patternMap)) {
+                        if (i++ > 1000) {
+                            break;
+                        }
+                        String data = "<div>" + escapeXml(
+                                line.priority + ";" + line.threadName + ";" + line.time + ";" + line.getContent())
+                                + "</div>";
                         String message = "event: log-message\n" +
                                 "data: " + data + "\n\n";
 
                         os.write(message.getBytes(StandardCharsets.UTF_8));
                         os.flush();
-//                        Thread.sleep(100);
+                        // Thread.sleep(100);
                     }
                 }
             } catch (InterruptedException e) {
@@ -114,5 +121,110 @@ public class Main {
             }
         }
         return result;
+    }
+
+    static Map<String, Pattern> parseFilter(String query, boolean isCaseInsensitive) {
+        String filterStr = query;
+        Map<String, Pattern> mapPattern = new HashMap<>();
+
+        int colStart = 0;
+        int colEnd = 0;
+        int condStart = 0;
+        int condEnd = 0;
+
+        try {
+            int regexFlag = 0;
+            // if (!filter.imIsCaseSensitive.get()) {
+            // regexFlag |= Pattern.CASE_INSENSITIVE;
+            // }
+            if (isCaseInsensitive) {
+                regexFlag |= Pattern.CASE_INSENSITIVE;
+            }
+
+            // Parsing logic could be done in regex instead
+            for (int i = 0; i < filterStr.length(); i++) {
+                char c = filterStr.charAt(i);
+                if (colEnd < 1) {
+                    for (; i < filterStr.length() && filterStr.charAt(i) == ' '; i++)
+                        ;
+                    colStart = i;
+                    for (; i + 1 < filterStr.length() && filterStr.charAt(i + 1) != ' '
+                            && filterStr.charAt(i + 1) != '='; i++)
+                        ;
+                    colEnd = i + 1;
+                } else if (condStart < 1) {
+                    if (c == '\'' || c == '"') {
+                        i = i + 1;
+                        condStart = i;
+                        for (; i < filterStr.length()
+                                && (filterStr.charAt(i) != '\'' && filterStr.charAt(i) != '"'); i++)
+                            ;
+                        condEnd = i;
+                        String colName = filterStr.substring(colStart, colEnd);
+                        Pattern pattern = Pattern.compile("^.*" + filterStr.substring(condStart, condEnd) + ".*$",
+                                regexFlag);
+                        mapPattern.put(colName.toUpperCase(), pattern);
+                    }
+                } else if (condEnd > 0) {
+                    if (i + 2 < filterStr.length()) {
+                        String s = filterStr.substring(i, i + 3);
+                        if (s.equalsIgnoreCase("AND")) {
+                            i = i + 3;
+                            for (; i + 1 < filterStr.length() && filterStr.charAt(i + 1) == ' '; i++)
+                                ;
+                            colStart = i + 1;
+                            colEnd = 0;
+                            condStart = 0;
+                            condEnd = 0;
+                        }
+                    }
+                }
+                for (; i + 1 < filterStr.length() && filterStr.charAt(i + 1) == ' '; i++)
+                    ;
+            }
+
+            if (mapPattern.isEmpty()) {
+                Pattern pattern = Pattern.compile("^.*" + filterStr + ".*$", regexFlag);
+                mapPattern.put("*", pattern);
+            }
+        } catch (PatternSyntaxException e) {
+            e.printStackTrace();
+            // filter.isRegexError = true;
+        }
+        return mapPattern;
+    }
+
+    static boolean isLineMatchFilter(ILogParser.LogDetail detail, Map<String, Pattern> mapPattern) {
+        if (mapPattern.isEmpty()) {
+            return true;
+        }
+        Pattern patternDefault = mapPattern.get("*");
+        boolean isAnyMatchTrue = mapPattern.size() == 1 && patternDefault != null;
+        Map<String, String> map = new HashMap<>();
+        map.put("C1", detail.priority);
+        map.put("LEVEL", detail.priority);
+        map.put("C2", detail.threadName);
+        map.put("THREAD", detail.threadName);
+        map.put("C3", detail.getContent());
+        map.put("CONTENT", detail.getContent());
+
+        for (Map.Entry<String, String> kv : map.entrySet()) {
+            Pattern pattern = mapPattern.getOrDefault(kv.getKey(), patternDefault);
+            if (pattern == null) {
+                continue;
+            }
+            String content = kv.getValue();
+            boolean isMatch = pattern.matcher(content).find();
+            if (!isAnyMatchTrue) {
+                if (!isMatch) {
+                    return false;
+                }
+            } else {
+                if (isMatch) {
+                    return true;
+                }
+            }
+        }
+        return !isAnyMatchTrue;
     }
 }
